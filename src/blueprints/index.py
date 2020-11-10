5106fdce3c77d0e84892c1b0dbb9e6061a3a3ed2
Raftor74 <raftor74@mail.ru>
2020-11-10 21:25:12 +0500

    init: Создан базовый каркас Flask приложения

---
+++ src/blueprints/index.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from utils.response import json_response
+
+bp = Blueprint('index', __name__)
+
+
+@bp.route('/')
+def index():
+    response = {'message': 'Hello World'}
+    return json_response.success(response)
